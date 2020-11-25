920773b4388d5155237ff447bb240b3d8480d34c
Jakub Senohrábek <jakub@senohrabek.net>
2020-11-25 20:00:03 +0100

    Automatické renderování registrace, úprava menu uživatele, změna form_theme na vertikální.

--- logos-polytechnikos.cz/src/Form/RegisterType.php
+++ logos-polytechnikos.cz/src/Form/RegisterType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -18,22 +19,41 @@ class RegisterType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options): void
 	{
 		$builder->setMethod('POST')
+            /*->add('title', TextType::class, [
+                'label' => 'Titul',
+                'attr' => [
+                    'maxlength' => 100,
+                ],
+                'required' => true,
+            ])*/
 			->add('name', TextType::class, [
-				'label' => 'jméno',
+				'label' => 'Jméno',
 				'attr' => [
 					'maxlength' => 100,
 				],
-				'help' => 'Zadejte všechna jména. Maximální délka je 100 znaků.',
 				'required' => true,
 			])
 			->add('surname', TextType::class, [
-				'label' => 'príjmení',
+				'label' => 'Příjmení',
 				'attr' => [
 					'maxlength' => 100,
 				],
-				'help' => 'Zadejte příjmení. Maximální délka je 100 znaků.',
 				'required' => true,
 			])
+            ->add('username', TextType::class, [
+                'label' => 'Přihlašovací jméno',
+                'attr' => [
+                    'maxlength' => 100,
+                ],
+                'required' => true,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'maxlength' => 100,
+                ],
+                'required' => true,
+            ])
 			->add('password', RepeatedType::class, [
 				'type' => PasswordType::class,
 				'invalid_message' => 'Hesla se neshodují, nebo nemají požadovanou složitost.',
@@ -48,8 +68,7 @@ class RegisterType extends AbstractType
 			->add('doRegister', SubmitType::class, [
 				'label' => 'Registrovat',
 				'attr' => [
-					'class' => 'btn btn-primary btn-sm',
-					'title' => 'Vytvoří uživatele',
+					'class' => 'btn btn-primary'
 				],
 			]);
 	}
