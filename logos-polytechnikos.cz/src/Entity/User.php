62d9d0c4d83953f6fe48a7ddf8867fc2288c0362
Jakub Senohrábek <jakub@senohrabek.net>
2020-12-17 19:31:46 +0100

    Opraveno UI přehledu recenzí pro šéfredaktora, v hlavní nabídce se zobrazuje jméno včetně titulů

--- logos-polytechnikos.cz/src/Entity/User.php
+++ logos-polytechnikos.cz/src/Entity/User.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\Entity;
 
@@ -16,248 +16,261 @@ use function unserialize;
 class User implements UserInterface, Serializable
 {
 
-	/**
-	 * @ORM\Id()
-	 * @ORM\GeneratedValue()
-	 * @ORM\Column(type="integer")
-	 * @var int|null
-	 */
-	private $id;
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @var int|null
+     */
+    private $id;
 
-	/**
-	 * @ORM\Column(type="string", length=20, unique=true, name="login")
-	 * @var string|null
-	 */
-	private $username;
+    /**
+     * @ORM\Column(type="string", length=20, unique=true, name="login")
+     * @var string|null
+     */
+    private $username;
 
-	/**
-	 * @ORM\Column(type="string", length=100)
-	 * @var string|null
-	 */
-	private $password;
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @var string|null
+     */
+    private $password;
 
-	/**
-	 * @ORM\Column(type="string", length=40, nullable=true)
-	 * @var string
-	 */
-	private $titleBeforeName;
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     * @var string
+     */
+    private $titleBeforeName;
 
-	/**
-	 * @ORM\Column(type="string", length=40, nullable=true)
-	 * @var string
-	 */
-	private $titleAfterName;
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     * @var string
+     */
+    private $titleAfterName;
 
-	/**
-	 * @ORM\Column(type="string", length=100)
-	 * @var string|null
-	 */
-	private $name;
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @var string|null
+     */
+    private $name;
 
-	/**
-	 * @ORM\Column(type="string", length=100)
-	 * @var string
-	 */
-	private $surname;
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @var string
+     */
+    private $surname;
 
-	/**
-	 * @ORM\Column(type="string", length=100, unique=true)
-	 * @var string|null
-	 */
-	private $email;
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     * @var string|null
+     */
+    private $email;
 
-	/**
-	 * @ORM\ManyToOne(targetEntity="App\Entity\UserRole", inversedBy="usersRole")
-	 * @var UserRole|null
-	 */
-	private $role;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\UserRole", inversedBy="usersRole")
+     * @var UserRole|null
+     */
+    private $role;
 
-	/**
-	 * @ORM\OneToMany(targetEntity="App\Entity\Review", mappedBy="reviewer")
-	 * @var Collection|Review[]|null
-	 */
-	private $reviews;
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Review", mappedBy="reviewer")
+     * @var Collection|Review[]|null
+     */
+    private $reviews;
 
-	/** @var string|null */
-	private $rolePlainText;
+    /** @var string|null */
+    private $rolePlainText;
 
-	public function __construct()
-	{
-		$this->reviews = new ArrayCollection();
-	}
+    public function __construct()
+    {
+        $this->reviews = new ArrayCollection();
+    }
 
-	public function getFullNameByName(): ?string
-	{
-		return $this->name . ' ' . $this->surname;
-	}
+    public function getFullName(): string
+    {
+        $fullName = '';
+        if ($this->titleBeforeName !== null && trim($this->titleBeforeName) !== '') {
+            $fullName .= $this->titleBeforeName . ' ';
+        }
+        $fullName .= $this->getFullNameByName();
+        if ($this->titleAfterName !== null && trim($this->titleAfterName) !== '') {
+            $fullName .= ', ' . $this->titleAfterName;
+        }
+        return $fullName;
+    }
 
-	public function getFullNameBySurname(): ?string
-	{
-		return $this->surname . ' ' . $this->name;
-	}
+    public function getFullNameByName(): ?string
+    {
+        return $this->name . ' ' . $this->surname;
+    }
 
-	/**
-	 * @return string[]
-	 */
-	public function getRoles(): array
-	{
-		return [
-			$this->role->getRole(),
-		];
-	}
+    public function getFullNameBySurname(): ?string
+    {
+        return $this->surname . ' ' . $this->name;
+    }
 
-	public function getSalt(): void
-	{
-		/** nutná implementace z interface */
-	}
+    /**
+     * @return string[]
+     */
+    public function getRoles(): array
+    {
+        return [
+            $this->role->getRole(),
+        ];
+    }
 
-	public function eraseCredentials(): void
-	{
-		/** nutná implementace z interface */
-	}
+    public function getSalt(): void
+    {
+        /** nutná implementace z interface */
+    }
 
-	public function serialize()
-	{
-		return serialize([
-			$this->id,
-			$this->username,
-			$this->email,
-			$this->password,
-			$this->titleBeforeName,
-			$this->titleAfterName,
-			$this->name,
-			$this->surname,
-		]);
-	}
+    public function eraseCredentials(): void
+    {
+        /** nutná implementace z interface */
+    }
 
-	public function unserialize($serialized): void
-	{
-		[
-			$this->id,
-			$this->username,
-			$this->email,
-			$this->password,
-			$this->titleBeforeName,
-			$this->titleAfterName,
-			$this->name,
-			$this->surname,
-		] = unserialize($serialized, ['allowed_classes' => false]);
-	}
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->email,
+            $this->password,
+            $this->titleBeforeName,
+            $this->titleAfterName,
+            $this->name,
+            $this->surname,
+        ]);
+    }
 
-	public function getId(): ?int
-	{
-		return $this->id;
-	}
+    public function unserialize($serialized): void
+    {
+        [
+            $this->id,
+            $this->username,
+            $this->email,
+            $this->password,
+            $this->titleBeforeName,
+            $this->titleAfterName,
+            $this->name,
+            $this->surname,
+        ] = unserialize($serialized, ['allowed_classes' => false]);
+    }
 
-	public function setId(?int $id): void
-	{
-		$this->id = $id;
-	}
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-	public function getUsername(): ?string
-	{
-		return $this->username;
-	}
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
 
-	public function setUsername(?string $username): void
-	{
-		$this->username = $username;
-	}
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
 
-	public function getPassword(): ?string
-	{
-		return $this->password;
-	}
+    public function setUsername(?string $username): void
+    {
+        $this->username = $username;
+    }
 
-	public function setPassword(?string $password): void
-	{
-		$this->password = $password;
-	}
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
 
-	public function getTitleBeforeName(): ?string
-	{
-		return $this->titleBeforeName;
-	}
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
 
-	public function setTitleBeforeName(?string $titleBeforeName): void
-	{
-		$this->titleBeforeName = $titleBeforeName;
-	}
+    public function getTitleBeforeName(): ?string
+    {
+        return $this->titleBeforeName;
+    }
 
-	public function getTitleAfterName(): ?string
-	{
-		return $this->titleAfterName;
-	}
+    public function setTitleBeforeName(?string $titleBeforeName): void
+    {
+        $this->titleBeforeName = $titleBeforeName;
+    }
 
-	public function setTitleAfterName(?string $titleAfterName): void
-	{
-		$this->titleAfterName = $titleAfterName;
-	}
+    public function getTitleAfterName(): ?string
+    {
+        return $this->titleAfterName;
+    }
 
-	public function getName(): ?string
-	{
-		return $this->name;
-	}
+    public function setTitleAfterName(?string $titleAfterName): void
+    {
+        $this->titleAfterName = $titleAfterName;
+    }
 
-	public function setName(?string $name): void
-	{
-		$this->name = $name;
-	}
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
 
-	public function getSurname(): ?string
-	{
-		return $this->surname;
-	}
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
 
-	public function setSurname(string $surname): void
-	{
-		$this->surname = $surname;
-	}
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
 
-	public function getEmail(): ?string
-	{
-		return $this->email;
-	}
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
 
-	public function setEmail(?string $email): void
-	{
-		$this->email = $email;
-	}
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
 
-	public function getRole(): ?UserRole
-	{
-		return $this->role;
-	}
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
 
-	public function setRole(?UserRole $role): void
-	{
-		$this->role = $role;
-	}
+    public function getRole(): ?UserRole
+    {
+        return $this->role;
+    }
 
-	public function getRolePlainText(): ?string
-	{
-		return $this->rolePlainText;
-	}
+    public function setRole(?UserRole $role): void
+    {
+        $this->role = $role;
+    }
 
-	public function setRolePlainText(?string $rolePlainText): void
-	{
-		$this->rolePlainText = $rolePlainText;
-	}
+    public function getRolePlainText(): ?string
+    {
+        return $this->rolePlainText;
+    }
 
-	/**
-	 * @return Review[]|Collection|null
-	 */
-	public function getReviews()
-	{
-		return $this->reviews;
-	}
+    public function setRolePlainText(?string $rolePlainText): void
+    {
+        $this->rolePlainText = $rolePlainText;
+    }
 
-	/**
-	 * @param Review[]|Collection|null $reviews
-	 */
-	public function setReviews($reviews): void
-	{
-		$this->reviews = $reviews;
-	}
+    /**
+     * @return Review[]|Collection|null
+     */
+    public function getReviews()
+    {
+        return $this->reviews;
+    }
+
+    /**
+     * @param Review[]|Collection|null $reviews
+     */
+    public function setReviews($reviews): void
+    {
+        $this->reviews = $reviews;
+    }
 
 }
